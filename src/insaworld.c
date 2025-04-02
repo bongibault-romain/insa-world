18589e516fe3c6ed8df5b2a7070f3f2b227236dc
root <root@insa-21121.insa-toulouse.fr>
2025-04-02 08:55:03 +0000

    question 1

--- src/insaworld.c
+++ src/insaworld.c
@@ -30,12 +30,12 @@ void sleep_until_next_activation(struct timespec *activation) {
   int err;
   do {
     err = clock_nanosleep(CLOCK_MONOTONIC, TIMER_ABSTIME, activation, NULL);
-    printf("err = %d\n", err);
   } while (err != 0 && errno != EINTR);
 } 
 
-void *thread_function() {
-  struct timespec period = {1,0}; // 1 seconds
+void *thread_function(void *arg) {
+  struct timespec *pperiod = (struct timespec *)arg; // 1 seconds
+  struct timespec period = *pperiod; // 1 seconds
   struct timespec next_activation;
 
   printf("Thread %ld started with period %ld.%09ld seconds\n", pthread_self(), period.tv_sec, period.tv_nsec);
@@ -63,11 +63,13 @@ void *thread_function() {
 
 int main(int argc, char *argv[]) {
   // pthread_t thread1,thread2;
+  struct timespec period = {1,0}; // 1 seconds
+
   // period1.tv_nsec = 0;
   // struct timespec period2 = {2,0}; // 2 seconds
   // pthread_create(&thread1, &period1, thread_function, NULL);
   // pthread_create(&thread2, &period2, thread_function, NULL);
 
 
-  thread_function();
+  thread_function(&period);
 }
